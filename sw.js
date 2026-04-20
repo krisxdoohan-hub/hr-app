8dab238415a0946156ae329b7c242bfdaf1112be
krisxdoohan-hub <krisx.doohan@gmail.com>
2026-04-20 22:50:58 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'hr-attendance-v1';
+const ASSETS = [
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/vue@3/dist/vue.global.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// 安裝時快取資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 攔截請求，優先從快取讀取 (實現斷網使用)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
